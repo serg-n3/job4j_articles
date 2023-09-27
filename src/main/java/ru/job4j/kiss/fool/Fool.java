8bf4091441f03849ab6eca20b0c7d46c6ea6e03d
serg-n3 <ktg80@yandex.ru>
2023-09-27 17:26:52 +0300

    2. Принципы Kiss, Dry и Yagni [#238813]

---
+++ src/main/java/ru/job4j/kiss/fool/Fool.java
@@ -0,0 +1,36 @@
+package ru.job4j.kiss.fool;
+
+import java.util.Scanner;
+
+public class Fool {
+
+    public static String answers(int number) {
+        String answer;
+        if (number % 3 == 0 && number % 5 == 0) {
+            answer = "FizzBuzz";
+        } else if (number % 3 == 0) {
+            answer = "Fizz";
+        } else if (number % 5 == 0) {
+            answer = "Buzz";
+        } else {
+           answer = String.valueOf(number);
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Игра FizzBuzz.");
+        var startAt = 1;
+        var io = new Scanner(System.in);
+        while (startAt < 100) {
+            System.out.println(answers(startAt));
+            startAt++;
+            var newAnswer = io.nextLine();
+            if (!answers(startAt).equals(newAnswer)) {
+                System.out.println("Ошибка. Начинай снова.");
+                startAt = 0;
+            }
+            startAt++;
+        }
+    }
+}
---
+++ src/test/java/ru/job4j/kiss/fool/FoolTest.java
@@ -0,0 +1,31 @@
+package ru.job4j.kiss.fool;
+
+import org.junit.jupiter.api.Test;
+import static ru.job4j.kiss.fool.Fool.answers;
+import static org.assertj.core.api.Assertions.*;
+
+class FoolTest {
+
+   @Test
+    public void whenNumberThree() {
+       String expected = "Fizz";
+       String result = answers(3);
+       assertThat(result).isEqualTo(expected);
+
+    }
+    @Test
+    public void whenNumberFive() {
+        String expected = "Buzz";
+        String result = answers(5);
+        assertThat(result).isEqualTo(expected);
+
+    }
+    @Test
+    public void whenNumberFifteen() {
+        String expected = "FizzBuzz";
+        String result = answers(15);
+        assertThat(result).isEqualTo(expected);
+
+    }
+  
+}
